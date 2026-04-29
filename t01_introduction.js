7826d58d9b55cd1eeb3b6224cdc3dd0a3fb9106d
stephan-5zabo <24285ss@hvhs.school.nz>
2026-04-29 02:32:06 +0000

    Apr 29, 2026, 2:32 AM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,12 +1,3 @@
-/****************************
-// Variables
-let Username = "Stephan";
-let Year = 2026;
-let age = 15;
-let money = 57;
-
-
-****************************/
 
 /****************************
 Name of task: Intro to Javascript
@@ -15,16 +6,3 @@ Name of task: Intro to Javascript
 
 console.log("Running t01_introduction.js");
 console.log("Yomie my homie")
-
-//Variables
-/****************************
-Main code
-****************************/
-
-
-
-
-
-/****************************
-Main code
-****************************/
--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,16 @@
-/* Paste your code from task 1 here */
+/****************************
+ Name of task; Variables
+****************************/
+
+
+// Variables
+let Username = "Stephan";
+let Year = 2026;
+let age = 15;
+let money = 57;
+
+
+/****************************
+Main Code
+****************************/
+console.log("hi" + username +)
