499ab66fb93d1b2847b618735c632252b62ae508
stephan-5zabo <24285ss@hvhs.school.nz>
2026-04-29 02:41:20 +0000

    Apr 29, 2026, 2:41 AM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,20 @@
-/* Paste your code from task 2 here */
+/****************************
+ Name of task; Variables
+****************************/
+let num1 = 10;
+let num2 = 5;
+
+console.log("add",    num1 + num2);
+console.log("subtract",)
+console.log("",)
+console.log("add",)
+/****************************
+Main Code
+****************************/
+
+
+
+
+/****************************
+Functions
+****************************/
