e7b398684df27bbd344dd814a1fd89dd5c0086b1
stephan-5zabo <24285ss@hvhs.school.nz>
2026-04-29 02:54:54 +0000

    Apr 29, 2026, 2:54 AM

--- t02_variables.js
+++ t02_variables.js
@@ -16,10 +16,10 @@ birthYear = Year - age;
 /****************************
 Main Code
 ****************************/
-console.log(" hi " + Username + " as of " + Year + " you are " + age + " years old. You have " + money + " dollars ")
-console.log("You were born in " + birthYear)
-console.log("in 10 years you will be")
-
+console.log(" hi " + Username + " as of " + Year + " you are " + age + " years old. You have " + money + " dollars ");
+console.log("You were born in " + birthYear);
+console.log("in 10 years you will be" + oldAge + " Years old.");
+console.log("you have" + money + " dollars")
 /****************************
 Functions
 ****************************/
