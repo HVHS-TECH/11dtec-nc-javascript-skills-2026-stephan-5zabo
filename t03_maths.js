26e944edc880bb40d9e73af6bf65f9c10c879946
stephan-5zabo <24285ss@hvhs.school.nz>
2026-04-29 02:44:02 +0000

    Apr 29, 2026, 2:44 AM

--- t03_maths.js
+++ t03_maths.js
@@ -4,14 +4,14 @@
 let num1 = 10;
 let num2 = 5;
 
-console.log("add",    num1 + num2);
-console.log("subtract",)
-console.log("",)
-console.log("add",)
+console.log("add ",    num1 + num2);
+console.log("subtract ",   num1 - num2);
+console.log("multiply ",    num1 * num2);
+console.log("devide ",    num1 / num2);
 /****************************
 Main Code
 ****************************/
-
+answer = num1 + num2
 
 
 
