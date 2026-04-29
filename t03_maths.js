b3ee61014b28641fa38ecaab47ebd69951d96a69
stephan-5zabo <24285ss@hvhs.school.nz>
2026-04-29 02:48:17 +0000

    Apr 29, 2026, 2:48 AM

--- t03_maths.js
+++ t03_maths.js
@@ -3,18 +3,23 @@
 ****************************/
 let num1 = 10;
 let num2 = 5;
+let answer;
 
 console.log("add ",    num1 + num2);
 console.log("subtract ",   num1 - num2);
 console.log("multiply ",    num1 * num2);
-console.log("devide ",    num1 / num2);
+console.log("divide ",    num1 / num2);
 /****************************
 Main Code
 ****************************/
-answer = num1 + num2
-
-
-
+answer = num1 + num2;
+console.log("add ",    answer);
+answer = num1 - num2;
+console.log("subtract ",    answer);
+answer = num1 * num2;
+console.log("multiply ",    answer);
+answer = num1 / num2;
+console.log("divide ",    answer);
 /****************************
 Functions
 ****************************/
