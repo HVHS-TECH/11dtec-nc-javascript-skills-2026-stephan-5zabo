a707655ae5d92eeb082d6d0f5b31ba9dfcd57be0
stephan-5zabo <24285ss@hvhs.school.nz>
2026-04-29 02:52:43 +0000

    Apr 29, 2026, 2:52 AM

--- t02_variables.js
+++ t02_variables.js
@@ -10,14 +10,15 @@ let age = 15;
 let money = 57;
 let birthYear;
 let oldAge;
-
+oldAge = age + 10;
+birthYear = Year - age;
 
 /****************************
 Main Code
 ****************************/
 console.log(" hi " + Username + " as of " + Year + " you are " + age + " years old. You have " + money + " dollars ")
-
-
+console.log("You were born in " + birthYear)
+console.log("in 10 years you will be")
 
 /****************************
 Functions
