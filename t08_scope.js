04821c7a4121a911855298c18e12c20c8bcad358
stephan-5zabo <24285ss@hvhs.school.nz>
2026-05-11 23:41:31 +0000

    May 11, 2026, 11:41 PM

--- t08_scope.js
+++ t08_scope.js
@@ -2,18 +2,18 @@ const output = document.getElementById("output");
 
 
 var myVar = 0;
-output.innerHTML = "Start: "+myVar+"<br>";
+output.innerHTML = "Start: "+ myVar +"<br>";
 
 functionOne();
 function functionOne() {
   myVar = 1;
-  output.innerHTML += "functionOne: "+myVar+"<br>";
+  output.innerHTML += "functionOne: "+ myVar +"<br>";
 }
 
 functionTwo();
 function functionTwo() {
   var myVar = 2;
-  output.innerHTML += "functionTwo: "+myVar+"<br>";
+  output.innerHTML += "functionTwo: "+ myVar +"<br>";
 }
 
-output.innerHTML += "end: "+myVar+"<br>";
+output.innerHTML += "end: "+ myVar +"<br>";
