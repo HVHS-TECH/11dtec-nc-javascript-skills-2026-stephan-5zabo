a76a57dee19423bacbc1d005f4eaaf57081dc2c5
stephan-5zabo <24285ss@hvhs.school.nz>
2026-05-14 21:22:01 +0000

    May 14, 2026, 9:22 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -1,1 +1,20 @@
-/* Paste your code from the last task */
+const output = document.getElementById("spaceForJavaScriptOutput");
+/****************************
+Main Code
+****************************/
+writeline("Less", "code", 8);
+displayWelcome("Stephan", 15)
+
+
+
+/****************************
+Functions
+****************************/
+function writeline(Less, code, matters){
+ output.innerHTML += "<p>"+Less + code + matters +"</p>";
+};
+
+function displayWelcome(_name, _age){
+    output. innerHTML += "<p> Welcome to the page "+_name+"</p>"
+    output. innerHTML += "<p>You are "+_age+" years old</p>"
+}
