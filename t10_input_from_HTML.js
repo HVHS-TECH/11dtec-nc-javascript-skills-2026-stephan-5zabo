4b2de20d898ae167f519f5e6faba23ecb3c002be
stephan-5zabo <24285ss@hvhs.school.nz>
2026-05-27 02:58:19 +0000

    May 27, 2026, 2:58 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -20,7 +20,7 @@ addMoney = halfMoney + 3;
 /****************************
 Main Code
 ****************************/
-const output = document.getElementById("spaceForJavaScriptOutput");
+const output = document.getElementById("1");
 
 /****************************
 Functions
@@ -34,6 +34,6 @@ Functions
 function getFormInput(){
     const NAME_FIELD = document.getElementById("Namefield")
      Username = NAME_FIELD.value;
-     console.log("<p> Your name is "+Username+" you goober </p>")
+     console.log("Your name is "+Username+" you goober.")
     output.innerHTML = "<p> Your name is "+Username+" you goober </p>";
 };
