7ba91f49a123f7e188948ec2951aff6b33127651
stephan-5zabo <24285ss@hvhs.school.nz>
2026-05-27 02:34:44 +0000

    May 27, 2026, 2:34 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -4,6 +4,16 @@
 console.log("running t01_javascriptAndHtml.js")
 
 // Variables
+let Username = "Stephan";
+let Year = 2026;
+let age = 15;
+let money = 57;
+let birthYear;
+let oldAge;
+oldAge = age + 10;
+birthYear = Year - age;
+halfMoney = money / 2;
+addMoney = halfMoney + 3;
 
 
 
