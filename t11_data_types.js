7fb2cafa48fd0b40e53d9f3c28f38fabcd12ef43
stephan-5zabo <24285ss@hvhs.school.nz>
2026-05-27 03:10:21 +0000

    May 27, 2026, 3:10 AM

--- t11_data_types.js
+++ t11_data_types.js
@@ -1,1 +1,38 @@
-/* Paste your code from the last task */
+/****************************
+ Name of task; activate via button
+****************************/
+console.log("running t01_javascriptAndHtml.js")
+
+// Variables
+let Username = "Stephan";
+let Year = 2026;
+let age = 15;
+let money = 57;
+let birthYear;
+let oldAge;
+oldAge = age + 10;
+birthYear = Year - age;
+halfMoney = money / 2;
+addMoney = halfMoney + 3;
+
+
+
+/****************************
+Main Code
+****************************/
+const output = document.getElementById("spaceForJavaScriptOutput");
+
+/****************************
+Functions
+
+
+
+****************************/
+
+
+
+function getFormInput(){
+    const NAME_FIELD = document.getElementById("Namefield")
+     Username = NAME_FIELD.value;
+    output.innerHTML = "<p> Your name is "+Username+" you goober </p>";
+};
