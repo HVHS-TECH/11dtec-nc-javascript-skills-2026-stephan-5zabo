773cf85ba0e9d1fbc6f937014bc40f11227e02d9
stephan-5zabo <24285ss@hvhs.school.nz>
2026-06-04 21:26:29 +0000

    Jun 4, 2026, 9:26 PM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -1,1 +1,44 @@
-/* Paste your code from the last task */
+/****************************
+ Name of task; conditionals
+****************************/
+console.log("running t01_javascriptAndHtml.js")
+
+// Variables
+let Username = "Stephan";
+let Year = 2026;
+let age = 15;
+let money = 57;
+let birthYear;
+let oldAge;
+oldAge = age + 10;
+birthYear = Year - age;
+halfMoney = money / 2;
+addMoney = halfMoney + 3;
+
+
+
+/****************************
+Main Code
+****************************/
+const output = document.getElementById("spaceForJavaScriptOutput");
+
+/****************************
+Functions
+
+
+
+****************************/
+
+
+
+function getFormInput(){
+    const NAME_FIELD = document.getElementById("Namefield")
+     Username = NAME_FIELD.value;
+     output.innerHTML += "<p> Your name is "+Username+" </p>";
+    const AGE_FIELD = document.getElementById("agefield")
+     age = AGE_FIELD.value;
+     output.innerHTML += "<p> you are "+age+" years old </p>";
+    const MONEY_FIELD = document.getElementById("moneyfield")
+     money = MONEY_FIELD.value;
+     output.innerHTML += "<p> And you have "+money+" pocket money left you goober </p>";
+};
